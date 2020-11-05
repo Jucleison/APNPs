f3b7ccd111887894f980ef29b3cc734e51ad7437
Juan <Jucleison09@gmail.com>
2020-11-05 19:07:51 -0300

    adição da rota mensagem

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 app.get('/',function(req,res){
    res.send("Ola Juan Alves")
 })
-
+app.get('/mensagem',function(req,res){
+    res.send("essa mensagem é automatica")
+})
 app.listen(3000,function(){
     console.log("Conexão inicializada")
 })
