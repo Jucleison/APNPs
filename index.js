379cf7f0b2ee32f5be51d9585a0c6974030bc424
Juan <Jucleison09@gmail.com>
2020-11-06 00:21:30 -0300

    a

--- index.js
+++ index.js
@@ -3,19 +3,37 @@ const app = express()
 var bodyParser = require('Body-parser')
 var cookieParser = require('cookie-parser')
 var path = require('path')
+const { ppid } = require('process')
 
 app.use(cookieParser())
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlincoded({extended:false}))
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.set("view engine","ejs")
 
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
-   res.send("Ola Juan Alves")
+    res.render('index.ejs',{})
 })
+
+app.get('/usuarios',function(req,res){
+    res.render('usuarios.ejs',{usuarios:[
+        {nome:'Juan', email:'Juan@gmail.com'},
+        {nome:'Maria', email:'maria@hotmail.com'},
+        {nome:'Tereza', email:'tereza@hotmail.com'},
+        {nome:'Ana', email:'ana@hotmail.com'}
+    ]})
+})
+app.get('/add',function(req,res){
+    res.render('adiciona.ejs')
+})
+
+app.post('/add', function(req,res){
+    console.log("Nome:" + req.body.txtNome + "Email: " + req.body.txtEmail)
+})
+
 
 app.listen(3000,function(){
     console.log("Conexão inicializada")
